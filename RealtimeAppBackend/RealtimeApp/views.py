e3ec29227460d31c457bf915a5291bd915493cdb
matthew-ardi <mardi@purdue.edu>
2018-11-30 21:00:43 -0500

    feat-backend: bluetooth data receiver

--- RealtimeAppBackend/RealtimeApp/views.py
+++ RealtimeAppBackend/RealtimeApp/views.py
@@ -8,6 +8,7 @@ import pusher
 import os
 import json
 import time
+import serial
 
 pusher_client = pusher.Pusher(
     app_id='659585',
@@ -42,7 +43,22 @@ def retrieveData(request):
 @csrf_exempt
 def retrieveBluetooth(request):
     # @TODO(matthew-ardi): Please put the code where you receive the data from bluetooth signal here and
+    s = serial.Serial('/dev/ttyS0', 115200, timeout=1)
+    string = ""
+    final_message = ""
+    while(1):
+        c = s.read()    # wait for start character
+        if c == '#':
+            for i in range (0,7):
+                c = s.readline()
+                string += c
+            break
+    
+    final_message = str(string.split('#', 1)[1])    # String of data transmitted through bluetooth from device
+
     # @TODO:               then modify the global object data
+
+    
     return
 
 
